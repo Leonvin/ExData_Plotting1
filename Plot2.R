23e48b010a698f48f3c67e9c88088efb657e52ba
Leonvin <vin.datascience@gmail.com>
2015-06-07 11:29:11 -0400

    Vin's exdata_plotting 1 assignment

---
+++ Plot2.R
@@ -0,0 +1,21 @@
+## Plot 2
+
+# reading the raw dataset
+raw_data <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                     nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+raw_data$Date <- as.Date(raw_data$Date, format="%d/%m/%Y")
+
+# Subsetting data from the dates 2007-02-01 and 2007-02-02
+data <- subset(raw_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(raw_data)
+
+# converting the Date and Time variables to Date/Time classes in R using the as.Date() function
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+# Creating the plot and saving it to png file
+windows()
+plot(data$Global_active_power~data$Datetime, type="l",
+     ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,35 @@
+## Plot 4
+
+# reading the raw dataset
+raw_data <- read.csv("./household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+                     nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
+raw_data$Date <- as.Date(raw_data$Date, format="%d/%m/%Y")
+
+# Subsetting data from the dates 2007-02-01 and 2007-02-02
+data <- subset(raw_data, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
+rm(raw_data)
+
+# converting the Date and Time variables to Date/Time classes in R using the as.Date() function
+datetime <- paste(as.Date(data$Date), data$Time)
+data$Datetime <- as.POSIXct(datetime)
+
+# Creating the plot and saving it to png file
+windows()
+par(mfrow=c(2,2), mar=c(4,4,2,1), oma=c(0,0,2,0))
+with(data, {
+  plot(Global_active_power~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  plot(Voltage~Datetime, type="l", 
+       ylab="Voltage (volt)", xlab="")
+  plot(Sub_metering_1~Datetime, type="l", 
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+  legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, bty="n",
+         legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+  plot(Global_reactive_power~Datetime, type="l", 
+       ylab="Global Rective Power (kilowatts)",xlab="")
+})
+
+dev.copy(png, file="plot4.png", height=480, width=530)
+dev.off()
